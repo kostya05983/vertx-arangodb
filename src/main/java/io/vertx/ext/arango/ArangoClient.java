8dfb1a560842fc433050ba55273d835caed30f95
kostya05983 <kostya05983@mail.ru>
2019-07-09 17:48:01 +0700

    [UPD] end with api client

--- src/main/java/io/vertx/ext/arango/ArangoClient.java
+++ src/main/java/io/vertx/ext/arango/ArangoClient.java
@@ -1,5 +1,6 @@
 package io.vertx.ext.arango;
 
+import com.arangodb.ArangoDatabase;
 import com.arangodb.entity.*;
 import com.arangodb.model.*;
 import io.vertx.codegen.annotations.Fluent;
@@ -16,6 +17,10 @@ public interface ArangoClient<T> {
     static ArangoClient createNonShared(Vertx vertx, JsonObject config, String dataSourceName) {
         return new ArangoClientImpl(vertx, config, dataSourceName);
     }
+
+    public ArangoDatabase db();
+
+    public String name();
 
     @Fluent
     public void insertDocument(String collectionName, T document,
@@ -100,4 +105,94 @@ public interface ArangoClient<T> {
     public void deleteDocuments(String collectionName, Collection<?> values, Class<T> type,
                                 DocumentDeleteOptions options,
                                 Handler<MultiDocumentEntity<DocumentDeleteEntity<T>>> resultHandler);
+
+    @Fluent
+    public void documentExists(String collectionName, String key, Handler<AsyncResult<Boolean>> resultHandler);
+
+    @Fluent
+    public void documentExists(String collectionName, String key, DocumentExistsOptions options,
+                               Handler<AsyncResult<Boolean>> resultHandler);
+
+    @Fluent
+    public void getIndex(String collectionName, String id, Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void deleteIndex(String collectionName, String id, Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void ensureHashIndex(String collectionName, Iterable<String> fields, HashIndexOptions options,
+                                Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void ensureSkiplistIndex(String collectionName, Iterable<String> fields, SkiplistIndexOptions options,
+                                    Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void ensurePersistentIndex(String collectionName, Iterable<String> fields, PersistentIndexOptions options,
+                                      Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void ensureGeoIndex(String collectionName, Iterable<String> fields, GeoIndexOptions options,
+                               Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void ensureFulltextIndex(String collectionName, Iterable<String> fields, FulltextIndexOptions options,
+                                    Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void getIndexes(String collectionName, Handler<AsyncResult<IndexEntity>> resultHandler);
+
+    @Fluent
+    public void exists(String collectionName, Handler<AsyncResult<Boolean>> resultHandler);
+
+    @Fluent
+    public void truncate(String collectionName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void count(String collectionName, Handler<AsyncResult<CollectionPropertiesEntity>> resultHandler);
+
+    @Fluent
+    public void create(String collectionName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void create(String collectionName, CollectionCreateOptions options,
+                       Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void drop(String collectionName, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    public void drop(String collectionName, boolean isSystem, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    public void load(String collectionName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void unload(String collectionName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void getInfo(String collectionName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void getProperties(String collectionName, Handler<AsyncResult<CollectionPropertiesEntity>> resultHandler);
+
+    @Fluent
+    public void changeProperties(String collectionName, CollectionPropertiesEntity options,
+                                 Handler<AsyncResult<CollectionPropertiesEntity>> resultHandler);
+
+    @Fluent
+    public void rename(String collectionName, String newName, Handler<AsyncResult<CollectionEntity>> resultHandler);
+
+    @Fluent
+    public void getRevision(String collectionName, Handler<AsyncResult<CollectionRevisionEntity>> resultHandler);
+
+    @Fluent
+    public void grantAccess(String collectionName, String user, Permissions permissions,
+                            Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    public void revokeAccess(String collectionName, String user, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    public void resetAccess(String collectionName, String user, Handler<AsyncResult<Void>> resultHandler);
 }
